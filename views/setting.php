d4c18705434898a9bbb2bd7f02c501cf38dd4db9
Flyerme <gao.eison@gmail.com>
2017-07-28 20:12:31 -0400

    Create setting.php

---
+++ views/setting.php
@@ -0,0 +1,24 @@
+<?php
+!defined('EMLOG_ROOT') && exit('access deined!');
+?>
+	
+
+					<!-- Basic Table -->
+					<div class="col-sm-12">
+						<div class="panel panel-default card-view">
+						
+
+
+
+
+<div id="tpl-options">
+	<div class="tpl-options-close">&laquo;返回</div>
+	<form action="<?php echo $this->url(array('template' => $template)); ?>" method="post" class="tpl-options-form">
+		<?php $this->renderOptions(); ?>
+	</form>
+</div>
+</div>
+</div>
+	
+
+
